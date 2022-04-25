87b948515802e698642be8f32c4ccae2cfd7a09d
Mike <michael.johanson@intel.com>
2022-04-25 12:30:10 -0700

    feat(build): adds support for c-shared buildmode

--- cmd/main.go
+++ cmd/main.go
@@ -8,24 +8,25 @@ import (
 	"fmt"
 	"os"
 	"rpc"
-	"rpc/internal/amt"
 	"rpc/internal/client"
 	"rpc/internal/rps"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func checkAccess() {
-	amt := amt.NewAMTCommand()
-	result, err := amt.Initialize()
-	if !result || err != nil {
-		println("Unable to launch application. Please ensure that Intel ME is present, the MEI driver is installed and that this application is run with administrator or root privileges.")
-		os.Exit(1)
-	}
+func runRPC(args []string) {
+	// process cli flags/env vars
+	flags := handleFlags(args)
+
+	startMessage := rps.PrepareInitialMessage(flags)
+
+	rpc := client.NewExecutor(*flags)
+	rpc.MakeItSo(startMessage)
 }
-func handleFlags() *rpc.Flags {
+
+func handleFlags(args []string) *rpc.Flags {
 	//process flags
-	flags := rpc.NewFlags(os.Args)
+	flags := rpc.NewFlags(args)
 	_, result := flags.ParseFlags()
 	if !result {
 		os.Exit(1)
@@ -43,15 +44,9 @@ func handleFlags() *rpc.Flags {
 	}
 	return flags
 }
-func main() {
 
+func main() {
 	// ensure we are admin/sudo
 	checkAccess()
-	// process cli flags/env vars
-	flags := handleFlags()
-
-	startMessage := rps.PrepareInitialMessage(flags)
-
-	rpc := client.NewExecutor(*flags)
-	rpc.MakeItSo(startMessage)
+	runRPC(os.Args)
 }
