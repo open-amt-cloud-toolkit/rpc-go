eddd467da3d090bd478d4c6db193a5334cc49e06
Tim Shockley <timothy.shockley@intel.com>
2023-08-03 11:06:13 -0700

    feat: normalized config file for wifi and 8021x

--- internal/config/local.go
+++ internal/config/local.go
@@ -5,6 +5,7 @@ type (
 		Password          string
 		IEEE8021XSettings `yaml:"ieee801xConfig"`
 		WifiConfigs       `yaml:"wifiConfigs"`
+		Ieee8021xConfigs  `yaml:"ieee8021xConfigs"`
 	}
 	IEEE8021XSettings struct {
 		Name                   string `yaml:"name"`
@@ -19,12 +20,16 @@ type (
 		PrivateKey             string `yaml:"privateKey"`
 	}
 	WifiConfigs []struct {
-		Priority               int    `yaml:"priority"`
+		ProfileName          string `yaml:"profileName"`
+		SSID                 string `yaml:"ssid"`
+		Priority             int    `yaml:"priority"`
+		AuthenticationMethod int    `yaml:"authenticationMethod"`
+		EncryptionMethod     int    `yaml:"encryptionMethod"`
+		PskPassphrase        string `yaml:"pskPassphrase"`
+		Ieee8021xProfileName string `yaml:"ieee8021xProfileName"`
+	}
+	Ieee8021xConfigs []struct {
 		ProfileName            string `yaml:"profileName"`
-		AuthenticationMethod   int    `yaml:"authenticationMethod"`
-		EncryptionMethod       int    `yaml:"encryptionMethod"`
-		SSID                   string `yaml:"ssid"`
-		PskPassphrase          string `yaml:"pskPassphrase"`
 		Username               string `yaml:"username"`
 		Password               string `yaml:"password"`
 		AuthenticationProtocol int    `yaml:"authenticationProtocol"`
