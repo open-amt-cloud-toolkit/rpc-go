f862477212ffb57b12f57e2a85c537445bda45e6
Mike Johanson <michael.johanson@intel.com>
2024-01-31 16:11:50 -0700

    localling it up

--- internal/local/amt/wsman.go
+++ internal/local/amt/wsman.go
@@ -2,7 +2,6 @@ package bacon
 
 import (
 	"encoding/base64"
-	"rpc/internal/lm"
 
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman/amt/general"
@@ -10,30 +9,6 @@ import (
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman/client"
 	"github.com/open-amt-cloud-toolkit/go-wsman-messages/pkg/wsman/ips/hostbasedsetup"
 )
-
-type LocalWsman struct {
-	local  lm.LocalMananger
-	data   chan []byte
-	errors chan error
-	status chan bool
-}
-
-func NewLocalWsman(username string, password string) LocalWsman {
-	lmDataChannel := make(chan []byte)
-	lmErrorChannel := make(chan error)
-	lmStatus := make(chan bool)
-	lm := LocalWsman{
-		local:  lm.NewLMEConnection(lmDataChannel, lmErrorChannel, lmStatus),
-		data:   lmDataChannel,
-		errors: lmErrorChannel,
-		status: lmStatus,
-	}
-	lm.local.Initialize()
-	return lm
-}
-func (l LocalWsman) Post(msg string) (response []byte, err error) {
-	return nil, nil
-}
 
 type WSMANer interface {
 	SetupWsmanClient(username string, password string)
@@ -56,12 +31,8 @@ func NewGoWSMANMessages(lmsAddress string) *GoWSMANMessages {
 	}
 }
 
-// implements TheInterface
-func (g *GoWSMANMessages) Unprovision(int) (setupandconfiguration.Response, error) {
-	return g.wsmanMessages.AMT.SetupAndConfigurationService.Unprovision(1)
-}
-
 func (g *GoWSMANMessages) SetupWsmanClient(username string, password string) {
+
 	clientParams := wsman.ClientParameters{
 		Target:    g.target,
 		Username:  username,
@@ -69,13 +40,16 @@ func (g *GoWSMANMessages) SetupWsmanClient(username string, password string) {
 		UseDigest: true,
 		UseTLS:    false,
 	}
-	if g.target != "local" {
-		wsmanClient := client.NewWsman(clientParams.Target, clientParams.Username, clientParams.Password, clientParams.UseDigest, clientParams.UseTLS, clientParams.SelfSignedAllowed)
-		g.wsmanMessages = wsman.NewMessages(wsmanClient)
-	} else {
-		wsmanClient := NewLocalWsman("", "")
-		g.wsmanMessages = wsman.NewMessages(wsmanClient)
-	}
+
+	wsmanClient := client.NewWsman(clientParams.Target, clientParams.Username, clientParams.Password, clientParams.UseDigest, clientParams.UseTLS, clientParams.SelfSignedAllowed)
+	lt := NewLocalTransport()
+	wsmanClient.Transport = lt
+
+	g.wsmanMessages = wsman.NewMessages(wsmanClient)
+	// } else {
+	// 	wsmanClient := NewLocalWsman("", "")
+	// 	g.wsmanMessages = wsman.NewMessages(wsmanClient)
+	// }
 }
 
 func (g *GoWSMANMessages) GetGeneralSettings() (general.Response, error) {
@@ -96,4 +70,8 @@ func (g *GoWSMANMessages) AddNextCertInChain(cert string, isLeaf bool, isRoot bo
 
 func (g *GoWSMANMessages) HostBasedSetupServiceAdmin(password, digestRealm string, nonce []byte, signature string) (hostbasedsetup.Response, error) {
 	return g.wsmanMessages.IPS.HostBasedSetupService.AdminSetup(hostbasedsetup.AdminPassEncryptionTypeHTTPDigestMD5A1, digestRealm, password, base64.StdEncoding.EncodeToString(nonce), hostbasedsetup.SigningAlgorithmRSASHA2256, signature)
+}
+
+func (g *GoWSMANMessages) Unprovision(int) (setupandconfiguration.Response, error) {
+	return g.wsmanMessages.AMT.SetupAndConfigurationService.Unprovision(1)
 }
