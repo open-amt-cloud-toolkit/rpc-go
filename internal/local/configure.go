4325959e69f96c76c51b251e586b250744e8d440
Matt Primrose <47540908+matt-primrose@users.noreply.github.com>
2024-03-19 15:57:29 -0700

    feat: adds amt features configuration (#407)
    Co-authored-by: madhavilosetty-intel <madhavi.losetty@intel.com>

--- internal/local/configure.go
+++ internal/local/configure.go
@@ -15,7 +15,17 @@ import (
 )
 
 func (service *ProvisioningService) Configure() (err error) {
+	// Check if the device is already activated
+	controlMode, err := service.amtCommand.GetControlMode()
+	if err != nil {
+		return utils.AMTConnectionFailed
+	}
+	if controlMode == 0 {
+		log.Error("Device is not activated to configure. Please activate the device first.")
+		return utils.UnableToConfigure
+	}
 	service.interfacedWsmanMessage.SetupWsmanClient("admin", service.flags.Password, logrus.GetLevel() == logrus.TraceLevel)
+
 	switch service.flags.SubCommand {
 	case utils.SubCommandAddEthernetSettings:
 		return service.AddEthernetSettings()
@@ -24,11 +34,21 @@ func (service *ProvisioningService) Configure() (err error) {
 	case utils.SubCommandEnableWifiPort:
 		return service.EnableWifiPort()
 	case utils.SubCommandSetMEBx:
+		if controlMode != 2 {
+			log.Error("Device needs to be in admin control mode to set MEBx password.")
+			return utils.UnableToConfigure
+		}
 		return service.SetMebx()
 	case utils.SubCommandConfigureTLS:
 		return service.ConfigureTLS()
 	case utils.SubCommandSyncClock:
 		return service.SynchronizeTime()
+	case utils.SubCommandSetAMTFeatures:
+		if controlMode != 2 {
+			log.Error("Device needs to be in admin control mode to configure AMT features.")
+			return utils.UnableToConfigure
+		}
+		return service.SetAMTFeatures()
 	default:
 	}
 	return utils.IncorrectCommandLineParameters
